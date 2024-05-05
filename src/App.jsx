f029ec39e348adf2e3df34a8278a0299a98b34c2
Ahmad Raza <razarajpoot99598@gmail.com>
2024-05-05 21:19:49 +0500

    New Project started! NetflixGPT

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className='text-4xl '>Netflix GPT project is starting!!!</h1>
+    
+    </>
+  )
+}
+
+export default App
